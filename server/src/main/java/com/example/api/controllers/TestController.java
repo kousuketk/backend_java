bba0c888cd4fc45991e299f71209b4b6d01827b6
kousuke takahashi <pamarikomi_t@icloud.com>
2021-06-17 10:38:56 +0900

    modify testController

--- server/src/main/java/com/example/api/controllers/TestController.java
+++ server/src/main/java/com/example/api/controllers/TestController.java
@@ -7,21 +7,31 @@ import org.springframework.web.bind.annotation.RestController;
 // import org.springframework.weg.bind.annotation*;でもいい
 
 @RestController
+@RequestMapping(path = "/test")
 public class TestController {
-    @RequestMapping(path = "/test", method = RequestMethod.GET)
-    public String test() {
-      return "ok from test.";
+    @RequestMapping(method = RequestMethod.GET)
+    public String getTest() {
+      return "get /test";
     }
 
-    @RequestMapping(path = "/test/json", method = RequestMethod.GET)
-    public Example testJson() {
-      var obj = new Example();
-      obj.value1 = "foo";
-      obj.value2 = "bar";
-      return obj;
+    // curl -X POST http://localhost:8080/test  
+    @RequestMapping(method = RequestMethod.POST)
+    public String postTest() {
+      return "post /test";
     }
 
-    @RequestMapping(path = "/test/json", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.PUT)
+    public String putTest() {
+      return "put /test";
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public String deleteTest() {
+      return "delete /test";
+    }
+
+    // curl http://localhost:8080/test/json -X POST -H 'Content-Type: application/json' -d '{"value1":"foo","value2":"bar"}' | jq .
+    @RequestMapping(path = "/example", method = RequestMethod.POST)
     public Example testPost(@RequestBody Example obj) {
       obj.value1 = obj.value1 + "_ok1";
       obj.value2 = obj.value2 + "_ok2";
