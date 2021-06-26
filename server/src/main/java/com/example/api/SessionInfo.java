3baf7cf51142aa543ecf47d4a5584c3e970ba5a2
kousuke takahashi <pamarikomi_t@icloud.com>
2021-06-26 14:43:08 +0900

    create session

--- server/src/main/java/com/example/api/SessionInfo.java
+++ server/src/main/java/com/example/api/SessionInfo.java
@@ -1,10 +1,42 @@
 package com.example.api;
 
-import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.io.Serializable;
 import java.util.Date;
 
-@Data
-public class SessionInfo {
-  private int userid;
-  private Data createdAt;
+@SessionScope
+@Component
+public class SessionInfo implements Serializable {
+
+  private static final long serialVersionUID = 8048097948251750715L;
+  private Integer userId;
+  private Date createdAt;
+  private String createdBy;
+
+  public Integer getUserId() {
+    return this.userId;
+  }
+
+  public void setUserId(Integer userId) {
+    this.userId = userId;
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt() {
+    this.createdAt = new Date();
+  }
+
+  public String getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy() {
+    this.createdBy = "created_by_setter";
+  }
+
 }
